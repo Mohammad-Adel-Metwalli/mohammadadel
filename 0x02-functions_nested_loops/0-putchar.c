9bec402202ffdc55835bfe506506d77d56fb28d2
TOKYO-FN <youssef.magdyii.12@gmail.com>
2023-07-18 07:26:36 -0700

    m

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Code for print putchar'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	char word[] = "_putchar";
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(word[i]);
+	}
+	_putchar('\n');
+	return (0);
+}

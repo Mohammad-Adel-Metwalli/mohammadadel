c5eb58261afef9aba9fe32d1fba30d58afce542f
TOKYO-FN <youssef.magdyii.12@gmail.com>
2023-07-18 07:56:16 -0700

    m

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,8 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Description: 'Code for print alpha'
- *
- * Return: Always 0 (Success)
+ * print_alphabet - utilizes on the _putchar function to print 
+ * the alphabet a - z
  */
 void print_alphabet(void)
 {

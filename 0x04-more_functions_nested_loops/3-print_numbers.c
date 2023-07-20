30421665f2576d026fed4393a93c1d2b9c59793a
TOKYO-FN <youssef.magdyii.12@gmail.com>
2023-07-20 04:11:49 -0700

    m

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,9 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * print_numbers - to print the numbers, from 0 to 9, followed by a new line
- *
- * Return: 0 (Success)
+ * print_numbers - a function that prints the numbers, from 0 to 9,
+ * followed by a new line.
+ * Return: Always 0
  */
 void print_numbers(void)
 {

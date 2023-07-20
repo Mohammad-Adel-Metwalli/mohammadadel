40489f865740b21b4cd6fce3b723e84a205a1e51
TOKYO-FN <youssef.magdyii.12@gmail.com>
2023-07-20 04:38:52 -0700

    m

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - to print the numbers, from 0 to 9, 
+ * print_most_numbers - to print the numbers, from 0 to 9,
  *
  * Return: numbers from 0 to 9
  */

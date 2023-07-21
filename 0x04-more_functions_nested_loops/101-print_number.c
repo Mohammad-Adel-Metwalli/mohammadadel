65ff4cd6837d3f837c5b2edbc1cb4ab0a1b9c135
TOKYO-FN <youssef.magdyii.12@gmail.com>
2023-07-20 17:45:53 -0700

    m

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,82 +1,25 @@
 #include "main.h"
 
-
-int _pow(int a, int b);
-
 /**
- * print_number - print the given number.
- * @n: number to print.
+ * print_number - a function that prints an integer.
+ *                using _putchar only
+ * @n: number
+ *
+ * Return: printed number
  */
 void print_number(int n)
 {
-	int collection = n;
-	unsigned int new_num;
-	int count = 0;
-	int print = 0;
+	unsigned int k = n;
 
 	if (n < 0)
 	{
-		new_num = -n;
+		n *= -1;
+		k = n;
 		_putchar('-');
 	}
-	else
-	{
-		new_num = n;
-	}
 
-	while (collection != 0)
-	{
-		collection /= 10;
-		++count;
-	}
-	count -= 1;
-
-	do {
-		if ((new_num / (_pow(10, count)) > 9) && count > 1)
-		{
-			print = new_num / (_pow(10, count));
-			print = print % 10;
-			_putchar('0' + print);
-		}
-		else if (count > 1)
-		{
-			print = (new_num / _pow(10, count));
-		       _putchar('0' + print);
-		}
-		else if (((new_num / 10) > 9) && count == 1)
-		{
-			print = (new_num / 10) % 10;
-			_putchar('0' + print);
-		}
-		else if (count == 1)
-		{
-			print = new_num / 10;
-			_putchar('0' + print);
-		}
-		else
-		{
-			print = new_num % 10;
-			_putchar('0' + print);
-		}
-		count--;
-	} while (count > -1);
-}
-
-/**
- * _pow - power fuction
- * @a: value to use.
- * @b: growth count
- *
- * Return: value int.
- */
-int _pow(int a, int b)
-{
-	int i;
-	int p = a;
-
-	for (i = 1 ; i < b ; i++)
-	{
-		a *= p;
-	}
-	return (a);
+	k /= 10;
+	if (k != 0)
+		print_number(k);
+	_putchar((unsigned int) n % 10 + '0');
 }

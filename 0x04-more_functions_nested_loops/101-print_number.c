e2154ff68ae4f2d3c8c80fbc13ac0eba4874d380
TOKYO-FN <youssef.magdyii.12@gmail.com>
2023-07-20 17:05:27 -0700

    m

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,36 +1,82 @@
-#include "main.h"
-void print_integer(int m);
+i#include "holberton.h"
+
+
+int _pow(int a, int b);
 
 /**
- * print_number - a function that prints an integer.
- * @n: An input integer
- * Return: Nothing
+ * print_number - print the given number.
+ * @n: number to print.
  */
 void print_number(int n)
 {
-	if (n == 0)
-		_putchar('0');
-	else if (n < 0)
+	int collection = n;
+	unsigned int new_num;
+	int count = 0;
+	int print = 0;
+
+	if (n < 0)
 	{
+		new_num = -n;
 		_putchar('-');
-		print_integer(n * -1);
 	}
 	else
-		print_integer(n);
+	{
+		new_num = n;
+	}
+
+	while (collection != 0)
+	{
+		collection /= 10;
+		++count;
+	}
+	count -= 1;
+
+	do {
+		if ((new_num / (_pow(10, count)) > 9) && count > 1)
+		{
+			print = new_num / (_pow(10, count));
+			print = print % 10;
+			_putchar('0' + print);
+		}
+		else if (count > 1)
+		{
+			print = (new_num / _pow(10, count));
+		       _putchar('0' + print);
+		}
+		else if (((new_num / 10) > 9) && count == 1)
+		{
+			print = (new_num / 10) % 10;
+			_putchar('0' + print);
+		}
+		else if (count == 1)
+		{
+			print = new_num / 10;
+			_putchar('0' + print);
+		}
+		else
+		{
+			print = new_num % 10;
+			_putchar('0' + print);
+		}
+		count--;
+	} while (count > -1);
 }
 
 /**
- * print_integer - A function to priting n
- * @m: an input unsigned integer
- * Return: Nothing
+ * _pow - power fuction
+ * @a: value to use.
+ * @b: growth count
+ *
+ * Return: value int.
  */
-void print_integer(int m)
+int _pow(int a, int b)
 {
-	int i = 1000000000;
+	int i;
+	int p = a;
 
-	for (; i >= 1; i /= 10)
-		if (m / i != 0)
-		{
-			_putchar((m / i) % 10 + '0');
-		}
+	for (i = 1 ; i < b ; i++)
+	{
+		a *= p;
+	}
+	return (a);
 }

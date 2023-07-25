ec6d961ad7809c838d5a1642962b3f7654bc88d5
TOKYO-FN <youssef.magdyii.12@gmail.com>
2023-07-24 19:24:16 -0700

    ☺

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - ☺
+ *
+ * @a: pointer
+ * @n: int
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	printf("%d\n", a[i]);
+}

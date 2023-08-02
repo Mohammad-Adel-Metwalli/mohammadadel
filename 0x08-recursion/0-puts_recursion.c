072b943adfa740146c889fe4e8b9387ce80a9791
TOKYO-FN <youssef.magdyii.12@gmail.com>
2023-08-02 08:50:01 -0700

    first commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a function that prints a string in recursive way
+ * 
+ * @s: pointer to character array
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (s[0] == '\0')
+		return;
+	_putchar(s[0]);
+	_puts_recursion(s+1);
+}

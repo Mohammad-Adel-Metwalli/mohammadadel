f6fc468d5e11b7b4b5d4cfbcbb7c682129568404
TOKYO-FN <youssef.magdyii.12@gmail.com>
2023-08-02 09:13:22 -0700

    .

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - a function that prints a rev of 
+ * _print_rev_recursion - a function that prints a rev of
  *                        a string in recursive way
  *
  * @s: pointer to character array

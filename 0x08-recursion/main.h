35c295b3b89564b15248982a48fc3e7a0638664b
TOKYO-FN <youssef.magdyii.12@gmail.com>
2023-08-02 16:08:21 -0700

    m

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -14,5 +14,7 @@ int is_prime_number(int n);
 int actual_sqrt_recursion(int n, int i);
 int is_prime(int n, int i);
 int check(char *s, int i, int len);
+int wildcmp(char *s1, char *s2);
+int is_palindrome(char *s);
 
 #endif

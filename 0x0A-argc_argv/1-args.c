3ef96cb6db7c6b8be6cebe3407f112b427f75517
TOKYO-FN <youssef.magdyii.12@gmail.com>
2023-08-04 18:12:46 -0700

    .

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc: number of arguments
+ * @argv: values of arguments
+ *
+ * Return: 0 On success
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}

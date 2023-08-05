831d8066d1066412e1ed91be685d78ec80cd239f
TOKYO-FN <youssef.magdyii.12@gmail.com>
2023-08-04 18:09:33 -0700

    .

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc: number of arguments
+ * @argv: values of arguments
+ *
+ * Return: 0 On success
+ */
+int main(int argc __attribute((unused)), char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

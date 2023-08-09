61a3e4e045e9b9362007969f70f3a0e3311959b2
TOKYO-FN <youssef.magdyii.12@gmail.com>
2023-08-08 18:41:03 -0700

    m

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,15 +28,26 @@ char *str_concat(char *s1, char *s2)
 	if (se == NULL)
 		return (NULL);
 
-	for (i = 0; i < len1 + len2; i++)
+	if (s1)
 	{
-		if (s1)
+		for (i = 0; i < len2; i++)
 		{
+			se[i] = s1[i];
+		}
+	}
+	else if (s2)
+		for (i = 0; i < len1; i++)
+		{
+			se[i] = s2[i];
+		}
+	else
+	{
+		for (i = 0; i < len1 + len2; i++)
+		{
+
 			if (i < len1)
 				se[i] = s1[i];
-		}
-		if (s2)
-		{
+
 			else
 				se[i] = s2[i - len1];
 		}

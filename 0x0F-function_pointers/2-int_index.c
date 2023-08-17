f2c94b0c4dfe6330cc66dca136b62e00dbf12fcc
TOKYO-FN <youssef.magdyii.12@gmail.com>
2023-08-16 17:46:13 -0700

    First Commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - a function that searches for an integer
+ *
+ * @array: array of integers
+ * @size: size of an array
+ * @cmp: function pointer
+ *
+ * Return: index (Success), -1 otherwise
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp[array[i]])
+				return (i);
+		}
+	}
+	return (-1);
+}

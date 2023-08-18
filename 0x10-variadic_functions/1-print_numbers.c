7f3463caf8e54c8e3bdc9013137d688958e28e95
TOKYO-FN <youssef.magdyii.12@gmail.com>
2023-08-17 18:34:22 -0700

    First Commit

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - a function that prints numbers, followed by a new line
+ *
+ * @separator: string sep
+ * @n: number of integers
+ *
+ * Return: print of numbers
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d%s", va_arg(args, int), separator);
+	}
+	printf("%d", va_arg(args, int));
+	putchar('\n');
+	va_end(args);
+}

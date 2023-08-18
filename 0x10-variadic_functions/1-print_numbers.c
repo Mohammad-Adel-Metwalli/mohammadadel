ae3827fc3f7b223088731115bf2cfb26fd03f907
TOKYO-FN <youssef.magdyii.12@gmail.com>
2023-08-17 18:45:33 -0700

    fix

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,9 +16,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	if (n == 0)
-		return;
-
 	for (i = 0; i < n - 1; i++)
 	{
 		if (separator != NULL)
@@ -27,6 +24,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			printf("%d", va_arg(args, int));
 	}
 	printf("%d", va_arg(args, int));
-	printf("\n");
 	va_end(args);
+	printf("\n");
 }
